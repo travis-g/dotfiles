c73fdefd83dc86a8eed2971d776b3d31fe9b444f
Travis Grammer <travisjgrammer@gmail.com>
2014-12-06 17:25:26 -0500

    Create holodeck.sh

---
+++ bin/holodeck.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+# 
+#   holodeck.sh - generate Star Trek holodeck background noise.
+#     Requires SoX and relevant libraries.
+#
+play -n -c2 synth whitenoise lowpass -1 69 lowpass -1 50 lowpass -1 120 gain +15
